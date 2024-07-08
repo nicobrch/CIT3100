4199e36104576a5c59c1044b5f35cb09e1c31651
Nico <nicolas.brch@gmail.com>
2024-07-08 15:38:43 -0400

    Establecer relaciones del modelo

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
 from sqlalchemy.sql import func
-from .db import Base
+from sqlalchemy.orm import relationship
+from db import Base
 
 class Admin(Base):
     __tablename__ = "Admin"
@@ -22,6 +23,8 @@ class Location(Base):
     location_city = Column(String, nullable=False)
     location_meta = Column(String, nullable=False)
 
+    company = relationship("Company", back_populates="locations")
+
 class Sensor(Base):
     __tablename__ = "Sensor"
     id = Column(Integer, primary_key=True, index=True)
@@ -31,9 +34,17 @@ class Sensor(Base):
     sensor_meta = Column(String)
     sensor_api_key = Column(String, unique=True, index=True)
 
+    location = relationship("Location", back_populates="sensors")
+
 class SensorData(Base):
     __tablename__ = "SensorData"
     id = Column(Integer, primary_key=True, index=True)
     sensor_id = Column(Integer, ForeignKey("sensors.id"))
     data = Column(Text, nullable=False)
     timestamp = Column(DateTime(timezone=True), server_default=func.now())
+
+    sensor = relationship("Sensor", back_populates="sensor_data")
+
+Company.locations = relationship("Location", order_by=Location.id, back_populates="Company")
+Location.sensors = relationship("Sensor", order_by=Sensor.id, back_populates="Location")
+Sensor.sensor_data = relationship("SensorData", order_by=SensorData.id, back_populates="Sensor")
