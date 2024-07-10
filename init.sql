a19066d52c4254cac75c99bac423ae6b543cea53
Nico <nicolas.brch@gmail.com>
2024-07-10 17:13:53 -0400

    Actualizar tablas init.sql

--- init.sql
+++ init.sql
@@ -1,40 +1,38 @@
-CREATE TABLE Admin (
+CREATE TABLE IF NOT EXISTS admin (
     username TEXT PRIMARY KEY,
     password TEXT NOT NULL
 );
 
-CREATE TABLE Company (
+CREATE TABLE IF NOT EXISTS company (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     company_name TEXT NOT NULL,
     company_api_key TEXT NOT NULL
 );
 
-CREATE TABLE Location (
+CREATE TABLE IF NOT EXISTS location (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     company_id INTEGER NOT NULL,
     location_name TEXT NOT NULL,
     location_country TEXT NOT NULL,
     location_city TEXT NOT NULL,
     location_meta TEXT,
-    FOREIGN KEY (company_id) REFERENCES Company(id)
+    FOREIGN KEY (company_id) REFERENCES company(id)
 );
 
-CREATE TABLE Sensor (
+CREATE TABLE IF NOT EXISTS sensor (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     location_id INTEGER NOT NULL,
     sensor_name TEXT NOT NULL,
     sensor_category TEXT NOT NULL,
-    sensor_meta TEXT,
     sensor_api_key TEXT NOT NULL,
-    FOREIGN KEY (location_id) REFERENCES Location(id)
+    sensor_meta TEXT,
+    FOREIGN KEY (location_id) REFERENCES location(id)
 );
 
-CREATE TABLE SensorData (
+CREATE TABLE IF NOT EXISTS sensordata (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     sensor_id INTEGER NOT NULL,
     data JSON NOT NULL,
     timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (sensor_id) REFERENCES Sensor(id)
+    FOREIGN KEY (sensor_id) REFERENCES sensor(id)
 );
-
-INSERT INTO Admin (username, password) VALUES ('admin', 'admin');
